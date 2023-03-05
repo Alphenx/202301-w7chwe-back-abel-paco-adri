9763be0179531dcb9b88ce3ad17a8a308df70f59
Abel <abelguevaralanda@yahoo.es>
2023-03-05 19:15:11 +0100

    change error globals code to error-handler file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import apiRouter from './api/api-router.js';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import authRouter from './api/auth/auth-router.js';
+import { authMiddleware } from './api/auth/auth-middleware.js';
+import { errorHandler } from './utils/error-handler.js';
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (_req, res) => {
   res.json('Hello world');
@@ -10,6 +15,9 @@ app.get('/', (_req, res) => {
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/v1', apiRouter);
+app.use('/auth', authRouter);
+app.use('/api/v1', authMiddleware, apiRouter);
+
+app.use(errorHandler);
 
 export default app;
